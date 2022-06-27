524e2f6d5d288886a52edc91047a3c04a9aaba9e
Antônio Santana <antonionetosg6@gmail.com>
2022-06-27 15:00:35 -0300

    Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -12,9 +12,11 @@ function compareTrue(parametro1, parametro2) {
 console.log(compareTrue(true, true))
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base, height) {
+
+  let resultado = (base * height) / 2;
+  return resultado;
+} console.log(calcArea(10, 50));
 
 // Desafio 3
 function splitSentence() {
