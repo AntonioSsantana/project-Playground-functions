5c9d588829bfbea20d290a6f906ecff610d48272
Antônio Santana <antonionetosg6@gmail.com>
2022-06-28 11:40:50 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -72,10 +72,20 @@ console.log(highestCount(arr))
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
+  
+  
+  if(Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
 
+    return 'cat1';
+  } else if (cat1 - mouse > cat2 - mouse ){
 
+    return 'cat2';
+  } else {
 
-}
+    return 'os gatos trombam e o rato foge'
+  }
+
+} console.log(catAndMouse(1, 0, 2))
 
 
 
